23144c062c7d0c6c67ca696f634c18048835be29
Iker Suen <suenik@oocl.com>
2018-12-03 14:24:11 +0800

    [iker] add hello world button and number:0 to app.js

--- src/App.js
+++ src/App.js
@@ -3,23 +3,11 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {number: 0}
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <button>Hello world!</button>number: {this.state.number}
       </div>
     );
   }
