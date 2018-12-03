7d959c71ba152eb6b5cfd5c56eabf0442534d778
Iker Suen <suenik@oocl.com>
2018-12-03 16:01:51 +0800

    [iker] feat: pass function from index.js to app.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  state = {number: 0}
-  upadteStateNumber = () => { this.setState({number: 1}) }
+  state = {number: this.props.defaultNumber}
+  upadteStateNumber = () => { this.setState({number: this.props.onClickHw(this.state.number)}) }
   render() {
     return (
       <div>
-        <button onClick= {this.upadteStateNumber}>Hello world!</button>number: {this.props.defaultNumber}
+        <button onClick= {this.upadteStateNumber}>Hello world!</button>number: {this.state.number}
       </div>
     );
   }
