378db16f86281f16acc62a6bd9f83d6193d6563a
Iker Suen <suenik@oocl.com>
2018-12-03 17:01:52 +0800

    [iker] feat: add class Line for import

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import Number from './components/Number.js'; 
+import Line from './components/Line.js'; 
 import './App.css';
 
 class App extends Component {
-  state = {number: this.props.defaultNumber}
-  upadteStateNumber = () => { this.setState({number: this.props.onClickHw(this.state.number)}) }
+  state = {
+    number: this.props.defaultNumber,
+    kyle: new Array(1).fill(0)
+  }
+  upadteStateNumber = () => { 
+    const temp = this.props.onClickHw(this.state.number)
+    this.setState({
+      number: this.props.onClickHw(this.state.number),
+      kyle: new Array(temp).fill(0)
+    }) 
+  }
   render() {
     return (
       <div>
         <button onClick= {this.upadteStateNumber}>Hello world!</button><Number /> {this.state.number}
+        {this.state.kyle.map(()=>(<Line />))}
       </div>
     );
   }
